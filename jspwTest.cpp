9aa8fa6d2cf76d83d5f6fa500cdd044b42594466
Abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-05-28 17:21:57 +0530

    neww

--- jspwTest.cpp
+++ jspwTest.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
 void print1(int n){
     for(int i=0; i<n; i++)
 	{
-		for(int j=0; j<n; j++)
+		for(int j=0; j<=i; j++)
 		{
-			cout<<"* ";
+			cout << "*";
 		}
-		cout<<endl;
+		cout << endl;
 	}
 }
 int main() {
- int t;
- cin >> t;
- for(i=0; i<t; i++){
-    int n;
-    cin>> n;
-    print1(n);
- }
-   
+   int n;
+   cin>>n;
+   print1(n);
 }
