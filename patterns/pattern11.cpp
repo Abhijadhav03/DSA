02fb7d1121c1c36a80efb998411b9eaf161b9f31
Abhishek Jadhav <jadhavabhishek53366@gmail.com>
2024-05-30 19:19:36 +0530

    pattern11

---
+++ patterns/pattern11.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void numberCrown(int n) {
+  
+  for(int i = 1; i <= n; i++) {
+      
+        for(int j = 1; j <= i; j++) {
+            cout << j<<" " ;
+        }
+
+       
+        int spaces = 2 * (n - i);
+        
+      
+        for(int j = 1; j <= spaces; j++) {
+            cout << "  ";
+        }
+
+    
+        for(int j = i; j >= 1; j--) {
+            cout << j<<" ";
+        }
+
+        cout << endl;
+    }
+}
+   int main() {
+   int n;
+   cin>>n;
+   numberCrown(n);
+   }
