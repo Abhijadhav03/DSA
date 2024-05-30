4ecc36c9f5f1e7aff6c6f4d4a3b3bebf4697b351
Abhishek Jadhav <jadhavabhishek53366@gmail.com>
2024-05-30 17:28:56 +0530

    pattern8

--- patterns/pattern8.cpp
+++ patterns/pattern8.cpp
@@ -1,3 +1,59 @@
+<<<<<<< HEAD
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+     for(int i=0; i<=n; i++)
+	{
+	
+        for (int j =0; j<n-i-1; j++)
+        {
+            cout <<" ";
+        }
+       
+      
+        for(int j=0;j< 2*i+1;j++){
+            
+            cout<<"*";
+        }
+        
+        
+         for (int j =0; j<n-i-1; j++)
+        {
+            cout <<" ";
+        }
+       
+	}
+    void print2(int n){
+    for(int i=0; i<n; i++)
+	{
+	
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+      
+        for(int j=0;j< 2*n -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+        
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        } 
+        cout<< endl;
+	}
+}}
+
+int main() {
+   int n;
+   cin>>n;
+   print1(n);
+   print2(n);
+=======
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -52,4 +108,5 @@ int main() {
    cin>>n;
    print1(n);
      print2(n);
+>>>>>>> f88abdca4cf656567cc2104d4aa6895d642f4403
 }
